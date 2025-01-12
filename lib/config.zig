6ccfc7feb90e50d1b4c5b112ceb1ec4ccf6836f1
MrBounty <adrien.bouvais.pro@gmail.com>
2025-01-12 19:08:34 +0100

    Send back to true and remove .pdb from release

--- lib/config.zig
+++ lib/config.zig
@@ -4,7 +4,7 @@ pub const CPU_CORE = 16;
 
 // Debug
 pub const PRINT_STATE = false;
-pub const DONT_SEND = true;
+pub const DONT_SEND = false;
 pub const DONT_SEND_ERROR = false;
 pub const RESET_LOG_AT_RESTART = false; // If true, will reset the log file at the start of the db, otherwise just keep adding to it
 
